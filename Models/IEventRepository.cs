8fef3a527e18093df3bdd4f8d4f3cb92fccc4b51
mj hughes <mhughes21@my.wctc.edu>
2020-01-20 20:03:09 -0600

    Add Event/Location data model

---
+++ Models/IEventRepository.cs
@@ -0,0 +1,11 @@
+﻿using Modas.Models;
+using System.Linq;
+
+namespace Modus.Models
+{
+    public interface IEventRepository
+    {
+        IQueryable<Event> Events { get; }
+        IQueryable<Location> Locations { get; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Modas.Models;
+using Modus.Models;
 
 namespace Modus
 {
@@ -15,6 +17,7 @@ namespace Modus
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IEventRepository, FakeEventRepository>();
             services.AddMvc();
         }
 
