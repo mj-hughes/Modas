0bf8277908374b5047af6079a100e503f4fdecee
Mary Hughes <mhughes21@my.wctc.edu>
2020-01-28 15:04:05 -0600

    02 Commit
    1) Create DB classes and configure EF core in class
    2) Create seed data in class
    3) Eager loading for location in class
    4) Create custom routing and pagination in class
    5) Add Bootstrap pagination footer per assignment
    6) Change startup for data migration/data seeding

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Modas.Models;
+using Modas.Models.ViewModels;
 
 namespace Modas.Controllers
 {
     public class HomeController : Controller
     {
-        //public ViewResult Index() => View();
+        private readonly int PageSize = 10;
+
         private IEventRepository repository;
         public HomeController(IEventRepository repo)
         {
             repository = repo;
         }
 
-        public ViewResult Index() => View(repository.Events);
+        // Uncustomized, default route http://localhost:47527/home/index?page=n
+        // Customized route http://localhost:47527/pagen
+        public ViewResult Index(int page = 1) 
+            => View(new EventsListViewModel 
+        {
+            Events = repository.Events
+                .Include(e => e.Location)
+                .OrderBy(e => e.TimeStamp)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize),
+            PagingInfo = new PagingInfo {
+                CurrentPage = page,
+                ItemsPerPage=PageSize,
+                TotalItems = repository.Events.Count()
+            }
+        });
+
     }
 }
---
+++ Models/ViewModels/EventsListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Modas.Models;
+
+namespace Modas.Models.ViewModels
+{
+    public class EventsListViewModel
+    {
+        public IEnumerable<Event> Events { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
